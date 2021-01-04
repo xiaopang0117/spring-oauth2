1abb3329bf86ee58a06a493fc5959a667e5a7c46
jiangkuan <jiangkuan@lixiang.com>
2021-01-04 15:51:01 +0800

    feat:授权服务器分发JWT令牌/资源服务器验签

--- auth-server/src/main/java/com/xiaopang/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/xiaopang/authserver/AuthServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthserverApplication {
+public class AuthServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthserverApplication.class, args);
+        SpringApplication.run(AuthServerApplication.class, args);
     }
 
 }
---
+++ resource-server/src/main/java/com/xiaopang/resourceserver/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.xiaopang.resourceserver.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class HelloController {
+
+    @PostMapping("/api/hi")
+    public String say(String name) {
+        return "hi , " + name;
+    }
+
+}
